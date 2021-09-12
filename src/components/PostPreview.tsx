13733b0aead883b4e9040ec2d605bb703914f3b6
DMG <dmwaigithinji@gmail.com>
2021-09-13 02:22:32 +0300

    feat: improve  posts ui

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,8 +1,14 @@
-import { ButtonBase, Grid, Paper, Typography } from "@material-ui/core";
+import { Box, ButtonBase, Grid, IconButton, Paper, Typography } from "@material-ui/core";
 import React, { ReactElement } from "react";
+
 import { Post } from "../API";
 import { useRouter } from "next/router";
 import { useUser } from "../context/AuthContext";
+
+import ThumbUp from "@material-ui/icons/ThumbUp";
+import ThumbDown from "@material-ui/icons/ThumbDown";
+
+import formatDatePosted from "../lib/formatDatePosted";
 
 interface Props {
   post: Post;
@@ -18,35 +24,48 @@ export default function PostPreview({ post }: Props): ReactElement {
     <Paper elevation={3}>
       <Grid
         container
-        direction="row"
+        direction="column"
         justifyContent="flex-start"
         alignItems="flex-start"
         wrap="nowrap"
-        spacing={3}
-        style={{ padding: 12, marginTop: 24 }}
+        spacing={2}
+        style={{ padding: "15px 30px", marginTop: 24 }}
       >
         {/* Content Preview */}
-        <Grid item>
+        <Grid item style={{ padding: "0 10px" }}>
           <ButtonBase onClick={() => router.push(`/post/${post.id}`)}>
             <Grid container direction="column" alignItems="flex-start">
-              <Grid item>
-                <Typography variant="body1">
-                  Posted by <b>{post.owner}</b>
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="h2">{post.title}</Typography>
-              </Grid>
-              <Grid
-                item
-                style={{
-                  maxHeight: 32,
-                  overflowY: "hidden",
-                  overflowX: "hidden",
-                }}
-              ></Grid>
+              <Typography variant="h2">{post.title}</Typography>
             </Grid>
           </ButtonBase>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1">{post.content}</Typography>
+        </Grid>
+
+        <Grid container direction="row" justifyContent="space-between" alignItems="center" wrap="nowrap">
+          {/* Metadata */}
+          <Grid container sm={12} style={{ padding: "0 10px" }}>
+            <Typography variant="body1">
+              by <b>{post.owner}</b>, <b>{formatDatePosted(post.createdAt)} hours ago</b>
+            </Typography>
+          </Grid>
+
+          {/* Upvote / votes / downvote */}
+          <Grid container sm={12} justifyContent="flex-end">
+            <Box display="flex" alignItems="center" style={{ paddingRight: "20px" }}>
+              <IconButton color="inherit" style={{ padding: "20px" }}>
+                <ThumbUp />
+              </IconButton>
+              <Typography variant="h6">{post.upvotes}</Typography>
+            </Box>
+            <Box display="flex" alignItems="center" style={{ paddingRight: "20px" }}>
+              <IconButton color="inherit" style={{ padding: "20px" }}>
+                <ThumbDown />
+              </IconButton>
+              <Typography variant="h6">{post.downvotes}</Typography>
+            </Box>
+          </Grid>
         </Grid>
       </Grid>
     </Paper>
